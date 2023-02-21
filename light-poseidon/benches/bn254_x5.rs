ca51b7e65c850f939f4a5e2ace1640aea37100c7
ananas-block <flow23@protonmail.com>
2023-02-21 20:44:21 +0000

    fixed bench

--- light-poseidon/benches/bn254_x5.rs
+++ light-poseidon/benches/bn254_x5.rs
@@ -7,11 +7,13 @@ use light_poseidon::{Poseidon, PoseidonHasher};
 
 pub fn bench_poseidon_bn254_x5(c: &mut Criterion) {
     let mut inputs = Vec::new();
-    for i in 2..18 {
+    for i in 1..17 {
         let name = [String::from("poseidon_bn254_x5_"), i.to_string()].concat();
         let random_bytes1 = Fr::from_be_bytes_mod_order(&rand::thread_rng().gen::<[u8; 32]>());
         inputs.push(random_bytes1);
-        c.bench_function(&name, |b| b.iter(|| Poseidon::<Fr>::hash(&inputs[..])));
+        let mut hasher = Poseidon::<Fr>::new_circom(i).unwrap();
+
+        c.bench_function(&name, |b| b.iter(|| hasher.hash(&inputs[..])));
     }
 }
 

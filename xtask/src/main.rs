4bfaa81dedfadb44f0590cc1aa8d5f7e6249e93a
Michal Rostecki <vadorovsky@protonmail.com>
2023-04-09 21:18:00 +0200

    Ensure that README.md content matches the rustdoc
    Add an xtask subcommand which autogenerates README.md from the rustdoc
    (from `src/lib.rs`) with a `--check` option, used in Github Actions as a
    part of a lint check.

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 
 mod generate_parameters;
+mod generate_readme;
 
 #[derive(Parser)]
 pub struct XtaskOptions {
@@ -11,6 +12,8 @@ pub struct XtaskOptions {
 #[derive(Parser)]
 enum Command {
     GeneratePoseidonParameters(generate_parameters::Options),
+    /// Generate the README.md file.
+    GenerateReadme(generate_readme::Options),
 }
 
 fn main() -> Result<(), anyhow::Error> {
@@ -20,6 +23,7 @@ fn main() -> Result<(), anyhow::Error> {
         Command::GeneratePoseidonParameters(opts) => {
             generate_parameters::generate_parameters(opts)?
         }
+        Command::GenerateReadme(opts) => generate_readme::generate_readme(opts)?,
     }
 
     Ok(())

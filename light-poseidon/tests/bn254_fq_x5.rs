291305320dd45da818e43706185d7d22b4d8170c
Mike Rostecki <vadorovsky@protonmail.com>
2023-08-07 18:46:18 +0200

    fix(core): Enforce strict length check for hashed bytes
    Ensure that each slice of bytes, provided as an input in
    `PoseidonBytesHasher`, has a size which matches the byte length of
    the modulus of the prime field, calculated as:
    
    ```
    (MODULUS_BIT_SIZE + 7) / 8
    ```
    
    Where modulus bit size is different for each prime field.
    
    The following methods from `ark_ff`, used usually for converting
    byte sequences to prime field elements, perform this check, but
    instead of returning an error, they simply take a subslice of the
    input, potentially leading to collisions:
    
    * `ark_ff::PrimeField::from_be_bytes_mod_order`
    * `ark_ff::PrimeField::from_le_bytes_mod_order`
    * `ark_ff::Field::from_random_bytes`
    
    `ark_ff` doesn't provide any method which would perform such check
    with returning a recoverable error.

--- light-poseidon/tests/bn254_fq_x5.rs
+++ light-poseidon/tests/bn254_fq_x5.rs
@@ -1,7 +1,32 @@
 use ark_bn254::Fr;
-use ark_ff::{BigInteger, One, PrimeField, Zero};
-use light_poseidon::Poseidon;
+use ark_ff::{BigInteger, BigInteger256, One, PrimeField, Zero};
+use light_poseidon::{Poseidon, PoseidonError};
 use light_poseidon::{PoseidonBytesHasher, PoseidonHasher};
+
+#[test]
+fn test_poseidon_one() {
+    let mut hasher = Poseidon::<Fr>::new_circom(2).unwrap();
+
+    let expected = [
+        0, 122, 243, 70, 226, 211, 4, 39, 158, 121, 224, 169, 243, 2, 63, 119, 18, 148, 167, 138,
+        203, 112, 231, 63, 144, 175, 226, 124, 173, 64, 30, 129,
+    ];
+
+    let input = Fr::from_be_bytes_mod_order(&[1u8]);
+    let hash = hasher.hash(&[input, input]).unwrap();
+
+    assert_eq!(hash.into_bigint().to_bytes_be(), expected,);
+
+    let input = Fr::from_be_bytes_mod_order(&[0u8, 1u8]);
+    let hash = hasher.hash(&[input, input]).unwrap();
+
+    assert_eq!(hash.into_bigint().to_bytes_be(), expected);
+
+    let input = Fr::from_be_bytes_mod_order(&[0u8, 0u8, 1u8]);
+    let hash = hasher.hash(&[input, input]).unwrap();
+
+    assert_eq!(hash.into_bigint().to_bytes_be(), expected);
+}
 
 #[test]
 fn test_poseidon_bn254_x5_fq_input_ones_twos() {
@@ -122,6 +147,164 @@ fn test_poseidon_bn254_x5_fq_hash_bytes_le() {
         ]
     );
 }
+
+#[cfg(feature = "fuzz-tests")]
+mod fuzz_tests {
+    use rand::Rng;
+
+    use super::*;
+
+    macro_rules! test_random_input_same_results {
+        ($name:ident, $method:ident) => {
+            #[test]
+            fn $name() {
+                let input = [1u8; 32];
+
+                for nr_inputs in 1..12 {
+                    let mut hasher = Poseidon::<Fr>::new_circom(nr_inputs).unwrap();
+
+                    let mut inputs = Vec::with_capacity(nr_inputs);
+                    for _ in 0..nr_inputs {
+                        inputs.push(input.as_slice());
+                    }
+
+                    let hash1 = hasher.$method(inputs.as_slice()).unwrap();
+                    let hash2 = hasher.$method(inputs.as_slice()).unwrap();
+
+                    assert_eq!(hash1, hash2);
+                }
+            }
+        };
+    }
+
+    test_random_input_same_results!(
+        test_poseidon_bn254_x5_fq_hash_bytes_be_random_input_same_results,
+        hash_bytes_be
+    );
+
+    test_random_input_same_results!(
+        test_poseidon_bn254_x5_fq_hash_bytes_le_random_input_same_results,
+        hash_bytes_le
+    );
+
+    macro_rules! test_invalid_input_length {
+        ($name:ident, $method:ident) => {
+            #[test]
+            fn $name() {
+                let mut rng = rand::thread_rng();
+
+                for _ in 0..100 {
+                    let len = rng.gen_range(33..524_288_000); // Maximum 500 MB.
+                    let input = vec![1u8; len];
+
+                    for nr_inputs in 1..12 {
+                        let mut hasher = Poseidon::<Fr>::new_circom(nr_inputs).unwrap();
+
+                        let mut inputs = Vec::with_capacity(nr_inputs);
+                        for _ in 0..nr_inputs {
+                            inputs.push(input.as_slice());
+                        }
+
+                        let hash = hasher.$method(inputs.as_slice());
+                        assert_eq!(
+                            hash,
+                            Err(PoseidonError::InvalidInputLength {
+                                len,
+                                modulus_bytes_len: 32,
+                            })
+                        );
+                    }
+                }
+            }
+        };
+    }
+
+    test_invalid_input_length!(
+        test_poseidon_bn254_x5_fq_hash_bytes_be_invalid_input_length,
+        hash_bytes_be
+    );
+
+    test_invalid_input_length!(
+        test_poseidon_bn254_x5_fq_hash_bytes_le_invalid_input_length,
+        hash_bytes_le
+    );
+
+    macro_rules! test_input_gt_field_size {
+        ($name:ident, $method:ident, $to_bytes_method:ident) => {
+            #[test]
+            fn $name() {
+                let mut greater_than_field_size = Fr::MODULUS;
+                let mut rng = rand::thread_rng();
+                let random_number = rng.gen_range(1u64..1_000_000u64);
+                greater_than_field_size.add_with_carry(&BigInteger256::from(random_number));
+                let greater_than_field_size = greater_than_field_size.$to_bytes_method();
+
+                assert_eq!(greater_than_field_size.len(), 32);
+
+                for nr_inputs in 1..12 {
+                    let mut hasher = Poseidon::<Fr>::new_circom(nr_inputs).unwrap();
+
+                    let mut inputs = Vec::with_capacity(nr_inputs);
+                    for _ in 0..nr_inputs {
+                        inputs.push(&greater_than_field_size[..]);
+                    }
+
+                    let hash = hasher.$method(inputs.as_slice());
+                    assert_eq!(hash, Err(PoseidonError::InputLargerThanModulus));
+                }
+            }
+        };
+    }
+
+    test_input_gt_field_size!(
+        test_poseidon_bn254_fq_hash_bytes_be_input_gt_field_size,
+        hash_bytes_be,
+        to_bytes_be
+    );
+
+    test_input_gt_field_size!(
+        test_poseidon_bn254_fq_hash_bytes_le_input_gt_field_size,
+        hash_bytes_le,
+        to_bytes_le
+    );
+}
+
+macro_rules! test_input_gt_field_size {
+    ($name:ident, $method:ident, $greater_than_field_size:expr) => {
+        #[test]
+        fn $name() {
+            for nr_inputs in 1..12 {
+                let mut hasher = Poseidon::<Fr>::new_circom(nr_inputs).unwrap();
+
+                let mut inputs = Vec::with_capacity(nr_inputs);
+                for _ in 0..nr_inputs {
+                    inputs.push(&$greater_than_field_size[..]);
+                }
+
+                let hash = hasher.$method(inputs.as_slice());
+                assert_eq!(hash, Err(PoseidonError::InputLargerThanModulus));
+            }
+        }
+    };
+}
+
+test_input_gt_field_size!(
+    test_poseidon_bn254_fq_hash_bytes_be_input_gt_field_size,
+    hash_bytes_be,
+    [
+        48, 100, 78, 114, 225, 49, 160, 41, 184, 80, 69, 182, 129, 129, 88, 93, 40, 51, 232, 72,
+        121, 185, 112, 145, 67, 225, 245, 147, 240, 0, 0, 2
+    ]
+);
+
+test_input_gt_field_size!(
+    test_poseidon_bn254_fq_hash_bytes_le_input_gt_field_size,
+    hash_bytes_le,
+    [
+        2, 0, 0, 240, 147, 245, 225, 67, 145, 112, 185, 121, 72, 232, 51, 40, 93, 88, 129, 129,
+        182, 69, 80, 184, 41, 160, 49, 225, 114, 78, 100, 48
+    ]
+);
 
 // test cases were created with circomlibjs poseidon([1, ...]) for 1 to 16 inputs
 const TEST_CASES: [[u8; 32]; 12] = [
